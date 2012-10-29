27359a2f38aec43ab7ed000635b88c533d073a11
cconoly <cconoly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-29 16:50:23 +0000

    dont need this
    git-svn-id: https://plugins.svn.wordpress.org/wp-flybox/trunk@618727 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$jk = file_get_contents('http://api.twitter.com/1/users/lookup.json?screen_name=wordpress');
-$jk = json_decode($jk);
-var_dump($jk);
-echo count($jk->ids);
-if ($jk->ids[0] > 0){echo 'yea';}
-?>
