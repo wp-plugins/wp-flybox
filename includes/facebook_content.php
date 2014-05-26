7b24f54563da56645d93b32ebb3580e9132061f5
cconoly <cconoly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-26 01:08:50 +0000

    trunk 5.5 added divs to iframes so they weren't effected by some responsive themes
    git-svn-id: https://plugins.svn.wordpress.org/wp-flybox/trunk@920887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/facebook_content.php
+++ includes/facebook_content.php
@@ -6,5 +6,7 @@ $show_faces=get_option('wpflybox_facebook_showfaces');
 $show_stream=get_option('wpflybox_facebook_stream');
 $show_header=get_option('wpflybox_facebook_header');
 $link=get_option('wpflybox_facebook');
-echo '<iframe title="Facebook" src="https://www.facebook.com/plugins/likebox.php?href='.urlencode($link).'&amp;width='.$fb_width.'&amp;height='.$fb_height.'&amp;colorscheme='.$fb_color.'&amp;show_faces='.$show_faces.'&amp;stream='.$show_stream.'&amp;header='.$show_header.'" style="background-color:'.get_option('wpflybox_facebook_background').'; overflow:hidden; width:'.$fb_width.'px; height:'.$fb_height.'px;border:1px solid '.get_option('wpflybox_facebook_border').'">iframes not supported by your browser.</iframe>';
+echo '<div style="background-color:'.get_option('wpflybox_facebook_background').'; overflow:hidden; width:'.$fb_width.'px; height:'.$fb_height.'px;border:1px solid '.get_option('wpflybox_facebook_border').';display:inline-block;">
+<iframe title="Facebook" src="https://www.facebook.com/plugins/likebox.php?href='.urlencode($link).'&amp;width='.$fb_width.'&amp;height='.$fb_height.'&amp;colorscheme='.$fb_color.'&amp;show_faces='.$show_faces.'&amp;stream='.$show_stream.'&amp;header='.$show_header.'" style="border:none;width:'.$fb_width.'px; height:'.$fb_height.'px;background-color:'.get_option('wpflybox_facebook_background').';">iframes not supported by your browser.</iframe>
+</div>';
 ?>
