e5fad31de691590f1e7effadb8a1dcafe102814b
cconoly <cconoly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-27 18:05:57 +0000

    tag 5.6 added tumblr tab
    git-svn-id: https://plugins.svn.wordpress.org/wp-flybox/tags/5.6@921785 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/es-admin.php
@@ -0,0 +1,195 @@
+<?php
+//Admin Language File
+$wpl_Optionssaved='Opciones guardadas.';
+$wpl_Cachesdeleted='En caché suprimida para la configuración actual.';
+//Display Options
+$wpl_DisplayOptions='Opciones de visualización';
+$wpl_Whichside='¿Qué lado?';
+$wpl_WhichsideDesc='Lado derecho hace que las barras de desplazamiento horizontales que aparezcan. He renunciado a arreglarlo. Si conoces a una solución por favor hágamelo saber!';
+$wpl_Hover='Flotar';
+$wpl_Click='Clic';
+$wpl_HoverOrClick='Pase el ratón o haga clic para abrir pestaña';
+$wpl_NoneOff='Ninguno/Fuera';
+$wpl_Left='Izquierda';
+$wpl_Right='Derecho';
+$wpl_Howhigh='¿Qué tan alto para empezar primera pestaña';
+$wpl_Amountofsep='Monto de la separación';
+$wpl_CompensateAdmin='¿Compensar bar miembro/administrador';
+$wpl_Yes='Sí';
+$wpl_No='No';
+$wpl_CompensateDesc='Seleccione Sí si desea que las lengüetas desplazan hacia abajo cuando la barra de admin / miembro está mostrando. Si la barra de admin / miembro no se muestra a continuación, no se desplazará. Seleccione No si no desea cambiar las pestañas de abajo cuando la barra de admin / miembro está mostrando. Si elige Sí tratará la parte inferior del menú / miembro como la parte superior de la página, donde la elección de No hará caso de la barra y el tratamiento de la parte superior de la barra como la parte superior de la página.';
+$wpl_Pushtabs='¿Empuje las pestañas hasta el borde de la pantalla?';
+$wpl_PushDesc='Si desea que los cuadros desplegables para estar en contra de la pantalla cuando están fuera elija Sí. Si desea que las cajas se deslice hacia fuera más allá de las pestañas de manera que las lengüetas no bloqueen el flyoutbox luego seleccione No.';
+$wpl_FloatTabs='¿Flotador aquí?';
+$wpl_FloatWithWindow='Flotador Con Ventana';
+$wpl_AttachedToPage='Adjunto a la página';
+$wpl_FloatDesc='Esta opción va a cambiar la forma de las lengüetas se comportan cuando se desplaza por la página. Si desea que las pestañas estén siempre en el lado de la página y luego elegir flotador con ventana. Si desea que las fichas para ser justo en la parte superior de la página y no se mueven con el movimiento en sentido vertical elija adjunto a la página. NOTA: attched generará barras de desplazamiento en la parte inferior de la página. Flotador con ventana se recomienda.';
+$wpl_Howmanytabs='¿Cuántas fichas?';
+$wpl_Apply='Aplicar';
+$wpl_Tab='Etiqueta';
+$wpl_Firsttab='Primera pestaña';
+$wpl_Secondtab='Segunda pestaña';
+$wpl_Thirdtab='Tercera pestaña';
+$wpl_Fourthtab='Cuarta pestaña';
+$wpl_Fifthtab='Quinta pestaña';
+$wpl_Sixthtab='Sexta pestaña';
+$wpl_Seventhtab='Séptima pestaña';
+$wpl_Eighthtab='Octava pestaña';
+$wpl_Notesamething='Nota: Asegúrese de que usted no tiene lo mismo por más de una pestaña o pueden no aparecer.';
+$wpl_Whichtypebutton='¿Qué tipo de botones?';
+$wpl_LogoandText='Logo y Texto';
+$wpl_CustomColors='Colores personalizados con logotipo';
+$wpl_BackgroundColor='Color de fondo (si está visible)';
+$wpl_BackgroundColorDesc='Use el nombre del color o el valor hexadecimal con el # delante.';
+$wpl_BorderColor='color del borde';
+$wpl_DeleteCacheAfterUpdate='Para algunos cambios tengan efecto, se deberá borrar la caché en el epígrafe "Other Options".';
+//Custom Button Configuration
+$wpl_CustomButtonConfig='Configuración de botón personalizado';
+$wpl_RightGradColor='Gradiente de color derecho';
+$wpl_LeftGradColor='Izquierda del color del gradiente';
+$wpl_BorderColor='Color del borde';
+$wpl_CurrentTabColors='Paleta de colores actuales';
+$wpl_IENoteCorners='Nota: Internet Explorer 8 y por debajo no soporta las esquinas redondeadas y sin el uso de archivos externos. Para los usuarios en Internet Explorer 8 y por debajo de las esquinas estarán cuadrado.';
+//Show Options
+$wpl_ShowOptions='Mostrar opciones';
+$wpl_MainPage='Página Principal';
+$wpl_Pages='Páginas';
+$wpl_Posts='Entradas';
+$wpl_Archives='Archivo';
+$wpl_Feeds='Feeds';
+$wpl_Search='búsqueda';
+$wpl_ForGuests='Para huéspedes';
+$wpl_ForUsers='Para los usuarios conectados';
+$wpl_IDsSep='IDs separados por comas';
+$wpl_ShowOnwith='Mostrar en Páginas / Mensajes con ID';
+$wpl_HideOnwith='Ocultar On Páginas / Mensajes con ID';
+$wpl_HelpFindingID='Ayuda para encontrar los ID';
+$wpl_Language='Idioma';
+$wpl_English='Inglés';
+$wpl_Spanish='Español';
+$wpl_LangDesc='Los archivos del idioma se pueden encontrar y editar en wp-content/plugins/wp-flybox/lang/.';
+$wpl_UpdateOptions='Opciones de actualización';
+$wpl_UpdateAllOptions='Actualizar todas las opciones';
+//Tab Configuration
+$wpl_TabConfig='Configuración pestaña';
+//Facebook
+$wpl_FullPageURL='URL de página completa';
+$wpl_FannotPersonal='Tiene que ser una página de fans, no una página personal.';
+$wpl_ColorScheme='Combinación de colores';
+$wpl_Light='Luz';
+$wpl_Dark='Oscuro';
+$wpl_Height='Altura';
+$wpl_Width='Ancho';
+$wpl_ShowHeader='Mostrar encabezado';
+$wpl_IsHeader='Encuéntrenos en Facebook es la cabecera';
+$wpl_ShowStream='Mostrar corriente';
+$wpl_ShowFaces='Mostrar caras';
+$wpl_fbscale='Escala para móvil';
+$wpl_fbscale_desc='Debe estar entre 0 y 1 (ejemplo 0.6).';
+//Twitter
+$wpl_TwitterUsername='Twitter Usuario';
+$wpl_UseCustomTwitter='Utilice Opciones personalizadas?';
+$wpl_CustomTwitterOptions='Opciones de Twitter personalizados';
+$wpl_CustomGiveErrors='Esto a veces puede dar errores. Gire Opciones personalizadas en No si lo hace.';
+$wpl_ShowWhat='Mostrar qué?';
+$wpl_Followers='Seguidores';
+$wpl_Friends='Amigos';
+$wpl_HowManyFF='El número de seguidores / amigos?';
+$wpl_Use0Friends='Utilice 0 si no desea mostrar seguidores o amigos.';
+$wpl_ProvideLinktoF='Proporcionar un enlace a Seguidores / Amigos';
+$wpl_On='En';
+$wpl_Off='Apagado';
+$wpl_TweettoButton='Tweet to button';
+$wpl_ShowLatestT='Ver últimos Tweet';
+//Google Plus
+$wpl_Googlenumuser='URL completo de la página de Google+';
+$wpl_Googleuserdesc='ejemplo: "https://plus.google.com/u/0/+WordPress/"';
+$wpl_PageType='Tipo de página';
+$wpl_Profile='Persona';
+$wpl_Page='Página';
+$wpl_Community='Comunidad';
+$wpl_GooglePagedesc='';
+$wpl_Layout='Disposición';
+$wpl_Landscape='Paisaje';
+$wpl_Portrait='Retrato';
+$wpl_GoogleTheme='Tema';
+$wpl_GoogleLight='Luz';
+$wpl_GoogleDark='Oscuro';
+$wpl_GoogleShowCover='Mostrar foto de presentación?';
+$wpl_GoogleShowTag='Mostrar línea de la etiqueta?';
+//Youtube
+$wpl_YoutubeUsername='Youtube nombre de usuario';
+//Subscription
+$wpl_FeedburnerUsername='Feedburner sitio de usuario / ID';
+$wpl_FeedburnerUsernameDesc='Vaya a "Editar Detalles de vínculo" en la página web de configuración FeedBurner. El usuario / ID será después http://feeds.feedburner.com/';
+//Pinterest
+$wpl_PinterestUsername='Pinterest nombre de usuario';
+$wpl_PinterestUserDesc='Se puede encontrar en la configuración de Pinterest.';
+//Linkdin
+$wpl_LinkedinID='URL usuario de LinkedIn o identificación de la compañía';
+$wpl_LinkedinFindID='Para encontrar su perfil público en LinkedIn URL, accede a LinkedIn. Haga clic en Perfil en la parte superior y seleccione vista de perfil. En la parte inferior de su información es la URL de su perfil público. <br> Para encontrar su look ID compañía en la barra de direcciones después de "http://www.linkedin.com/company/" será su ID de empresa.';
+$wpl_LinkedinMember='Miembro o de perfil de la compañía';
+$wpl_Member='Miembro';
+$wpl_Company='Empresa';
+//Flickr
+$wpl_FlickrID='Flickr ID';
+$wpl_FlickerIDGetter='Use esta URL para ayudarle a encontrar su ID';
+$wpl_PhotoTag='Etiqueta fotográfica';
+$wpl_PhotoTagDesc='Mostrar sólo las fotos con esta etiqueta, o dejar en blanco para todos.';
+//Contact
+$wpl_ContactMe='Ponte en contacto conmigo';
+$wpl_CompletedForm='Enviar para enviar el formulario completado a';
+$wpl_UseCaptcha='Utilice Captcha';
+$wpl_TabWording='redacción pestaña';
+$wpl_ContactUs='Contáctenos';
+//DeviantART
+$wpl_DeviantUser='DeviantART Nombre de usuario';
+$wpl_DevFrameWidth='ancho de vía';
+$wpl_DevFramHeight='Altura del cuadro';
+$wpl_DevMaxNum='Número máximo de mostrar';
+$wpl_DevMaxNumDesc='El número máximo de imágenes para mostrar en el marco de';
+$wpl_DevMaxWidth='Anchura máxima de cada imagen';
+$wpl_DevMaxWidthDesc='(Ancho máximo que desea de cada imagen)(0-150)';
+$wpl_DevMaxHeight='Altura máxima de cada imagen';
+$wpl_DevMaxHeightDesc='(Ancho máximo que desea de cada imagen) (0-150) Deja en blanco para';
+$wpl_DevCol='Número de columnas';
+//Instagram
+$wpl_ToAuthorize='WP-Flybox necesita ser autorizado primero por lo que no tiene permiso para mostrar sus fotos. <br /> Para hacerlo vaya <a href = "http://njarb.com/wp-flybox/instagram" target = "_blank" > aquí </ a>.';
+$wpl_InstUserID='ID de usuario';
+$wpl_InstGetIDHere='Recibe su nombre de aquí.';
+$wpl_InstAuth='Token de autorización para WP-Flybox';
+$wpl_InstGetAuthHere='Obtenga su autenticación Token aquí.';
+$wpl_InstMaxRecent='Número máximo de fotos recientes';
+$wpl_InstHowManyPics='¿Cuántas fotos es lo que quieres mostrar?';
+//Vimeo
+$wpl_VimUser='Vimeo Nombre de usuario';
+$wpl_VimVideotoShow='vídeos para mostrar';
+$wpl_VimZerotoShowNo='0 sin mostrar vídeos';
+//Troubleshoot
+$wpl_Troubleshoot='Solucionar problemas';
+$wpl_MobileSupport='soporte móvil';
+$wpl_EnableForMobile='Habilitar para dispositivos móviles';
+$wpl_IpadSupport='soporte iPad';
+$wpl_EnableforIpad='Habilitar para iPads';
+$wpl_Caching='El almacenamiento en caché';
+$wpl_CacheDesc='Dado que la mayoría de estas fichas requieren datos de otro sitio como vimeo.com, twitter.com etc, puede tardar un poco en cargar. Todas las fichas que no se cargan de forma asíncrona a través de JavaScript se almacenan en caché en wordpress. Las fichas que se almacenan en caché son Twitter, Deviant Art, Instagram y Vimeo. La primera vez que cargue su sitio va a tardar un poco más si se utiliza una de estas fichas en caché porque tiene que obtener los datos en otro lugar y lo guarda, pero después de que se almacena en caché se carga al instante sin necesidad de una conexión a otro sitio. Los cachés duran aproximadamente 6-12 horas. Si se cambia un valor y no se ha actualizado en su página, a continuación, intente borrar la memoria caché y forzar WP-Flybox para almacenar en caché con la nueva configuración. Se recomienda sólo para borrar las cachés si usted está experimentando problemas.';
+$wpl_DeleteCaches='Borrar cachés?';
+$wpl_ChecktoDelete='Compruebe borrar caches';
+$wpl_AutoDisIE='Nota: Este plugin se desactiva automáticamente para Internet Explorer 7.0 y por debajo, porque este plugin utiliza css deslizamiento avanzada no se admite en las versiones anteriores de IE.';
+//Sidebar
+$wpl_BlogPost='entrada de blog';
+$wpl_WordpressPluginPage='Página de plugin para Wordpress';
+$wpl_Help='ayudar';
+$wpl_Rate='velocidad';
+//Custom Content
+$wpl_CustomContent='Código HTML personalizado';
+$wpl_CustomContentDesc='';
+$wpl_Custom='costumbre';
+$wpl_CustomIcon='icono personalizado';
+$wpl_CustomIconDesc='Estos sólo serán utilizados si la ubicación de encargo debajo está en blanco. Úselos cuando utilice colores personalizados logotipo botones.';
+$wpl_CustomIconUpload='Ubicación del icono personalizado';
+$wpl_CustomIconUploadDesc='Dejar en blanco si se utiliza un icono de arriba. Esta imagen será escalada hasta 30px de ancho. No se olvide de incluir http://';
+//Tumblr
+$wpl_Tumblr='Tumblr URL del blog';
+$wpl_Tumblr_Desc='URL sin http://';
+$wpl_Tumblr_Max='Máximo imágenes que mostrar';
