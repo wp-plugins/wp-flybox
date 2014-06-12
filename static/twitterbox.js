1bc76c0af017c2cc82d857bf4b61a207764d9ff6
cconoly <cconoly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-12 13:38:51 +0000

    tags/5.8 check if id tab exists before fetching
    git-svn-id: https://plugins.svn.wordpress.org/wp-flybox/tags/5.8@931137 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ static/twitterbox.js
@@ -0,0 +1,2 @@
+//Dowloaded from http://script-ot.googlecode.com/svn/twitterbox.js on August 7, 2012
+function fanbox_init(screen_name){document.getElementById('twitterfanbox').innerHTML='\<iframe name=\"fbfanIFrame_0\" frameborder=\"0\" allowtransparency=\"true\" src=\"http://s.moopz.com/connect.html?user='+screen_name+'\" class=\"FB_SERVER_IFRAME\" scrolling=\"no\" style=\"width: 232px; height: 237px; border-top-style: none; border-right-style: none; border-bottom-style: none; border-left-style: none; border-width: initial; border-color: initial; \"\>\<\/iframe\>';}
