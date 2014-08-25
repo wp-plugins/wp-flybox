ed4ba545e32000d72b46ee6b29ccb54580e25d4e
cconoly <cconoly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 19:14:32 +0000

    5.13 tag - custom button for each tab by URL
    git-svn-id: https://plugins.svn.wordpress.org/wp-flybox/tags/5.13@972616 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/deviant_tab.php
@@ -0,0 +1,16 @@
+<?php
+$wpflybox_my_deviant_tab=get_option('wpflybox_my_deviant_tab');
+if ($right_or_left=='right'){$right_or_left_cap='Right';}else{$right_or_left_cap='Left';}
+if ($wpflybox_my_deviant_tab!='')
+  {
+  echo '<img src="'.$wpflybox_my_deviant_tab.'" height="32" width="32" alt="F">';
+  }else 
+  {
+  if ($use_custom_button=='true')
+    {
+    echo '<img src="'.$plugin_url.'/wp-flybox/static/icons/deviant.png" height="30" width="30" alt="F">';
+    }else{
+    echo '<div style="margin:0px; width:33px; height:101px; background-position:0px -909px; background-image:url(\''.$plugin_url.'/wp-flybox/static/FlyBoxSprite'.$right_or_left_cap.'.png\');padding:0px;"> </div>';
+    }
+  }
+?>
